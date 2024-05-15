534fe6ceb3337adad18a5fbf70b9363a1485bf97
catherinefalvo <69995696+catherinefalvo@users.noreply.github.com>
2024-05-15 21:40:31 +0000

    cepvalidator

---
+++ src/cepValidator.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const validateCepFormat = (cep) => {
+  const regex = /^[0-9]{5}-[0-9]{3}$/;
+  return regex.test(cep);
+};
+
+const getCepInfo = async (cep) => {
+  if (!validateCepFormat(cep)) {
+    throw new Error('Invalid CEP format');
+  }
+  const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+  return response.data;
+};
+
+module.exports = { validateCepFormat, getCepInfo };
