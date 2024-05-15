8ad073cd1d18ca61c10534da5d5e36525a9721ac
catherinefalvo <69995696+catherinefalvo@users.noreply.github.com>
2024-05-15 21:55:23 +0000

    cenarios de teste

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,11 +1,31 @@
 const { getCepInfo } = require('../src/cepValidator');
 
-test('fetch CEP info', async () => {
-  const data = await getCepInfo('01001-000');
-  expect(data).toHaveProperty('cep', '01001-000');
-  expect(data).toHaveProperty('logradouro', 'Praça da Sé');
+test('Cenario CEP Motuca', async () => {
+  const data = await getCepInfo('14835-000');
+  expect(data).toHaveProperty('cep', '14835-000');
+  expect(data).toHaveProperty('logradouro', '');
+  expect(data).toHaveProperty('complemento', '');
+  expect(data).toHaveProperty('bairro', '');
+  expect(data).toHaveProperty('localidade', 'Motuca');
+  expect(data).toHaveProperty('uf', 'SP');
+  expect(data).toHaveProperty('ddd', '16');
 });
 
-test('invalid CEP format throws error', async () => {
+test('Cenario CEP Araraquara', async () => {
+    const data = await getCepInfo('14801-788');
+    expect(data).toHaveProperty('cep', '14801-788');
+    expect(data).toHaveProperty('logradouro', 'Avenida Paulino Rodella');
+    expect(data).toHaveProperty('complemento', 'de 630 ao fim - lado par');
+    expect(data).toHaveProperty('bairro', 'Jardim das Flores');
+    expect(data).toHaveProperty('localidade', 'Araraquara');
+    expect(data).toHaveProperty('uf', 'SP');
+    expect(data).toHaveProperty('ddd', '16');
+  });
+
+test('Cenario CEP Invalido', async () => {
   await expect(getCepInfo('01001000')).rejects.toThrow('Invalid CEP format');
 });
+
+test('Cenario CEP Invalido (letras) ', async () => {
+    await expect(getCepInfo('a1001-b00')).rejects.toThrow('Invalid CEP format');
+  });
