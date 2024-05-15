c9d1cfefe6e8bc40f0e45b1954ae4c1b075c7853
catherinefalvo <69995696+catherinefalvo@users.noreply.github.com>
2024-05-15 21:55:02 +0000

    Cenarios de teste

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -1,10 +1,35 @@
 const { validateCepFormat } = require('../src/cepValidator');
 
-test('valid CEP format', () => {
+test('Cenário CEP Válido', () => {
   expect(validateCepFormat('01001-000')).toBe(true);
 });
 
-test('invalid CEP format', () => {
-  expect(validateCepFormat('01001000')).toBe(false);
-  expect(validateCepFormat('ABCDE-123')).toBe(false);
+test('Cenário CEP Inválido (Vazio)', () => {
+  expect(validateCepFormat('')).toBe(false);
+});
+
+test('Cenário CEP Inválido (Menos Números)', () => {
+    expect(validateCepFormat('1')).toBe(false);
+    expect(validateCepFormat('14')).toBe(false);
+    expect(validateCepFormat('148')).toBe(false);
+    expect(validateCepFormat('1480')).toBe(false);
+    expect(validateCepFormat('14801')).toBe(false);
+    expect(validateCepFormat('14801-')).toBe(false);
+    expect(validateCepFormat('14801-0')).toBe(false);
+    expect(validateCepFormat('14801-00')).toBe(false);    
+});
+
+test('Cenário CEP Inválido (Mais Números)', () => {
+    expect(validateCepFormat('14801-0000')).toBe(false);   
+    expect(validateCepFormat('148010-000')).toBe(false);   
+});
+
+test('Cenário CEP Inválido (Letras)', () => {
+    expect(validateCepFormat('abcde-fgh')).toBe(false);  
+    expect(validateCepFormat('a0111-f00')).toBe(false);    
+});
+
+test('Cenário CEP Inválido (Sem Traço)', () => {
+    expect(validateCepFormat('14801788')).toBe(false);  
+    expect(validateCepFormat('14835000')).toBe(false);    
 });
